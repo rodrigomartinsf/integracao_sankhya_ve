f61b9e3f97ba4f53d435a42d322be105816ec0c8
Rodrigo Martins <Rodrigo.MartinsFernandes@fit-tecnologia.org.br>
2022-08-23 17:04:12 -0400

    alteracao da forma de autenticacao

--- sankhya/classes/auth.py
+++ sankhya/classes/auth.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import os
 
 class Auth:
 
@@ -14,10 +15,10 @@ class Auth:
 
   def __init__(self):
     self.jsessionid = None
-    self.user = 'SUP'
-    self.password = '$CB!vy8@ZO'
+    self.user =  os.environ.get('SANKHYA_USER')
+    self.password =  os.environ.get('SANKHYA_PASSWORD')
     self.url_login = "http://navecunha.nuvemdatacom.com.br:9665/mge/service.sbr?serviceName=MobileLoginSP.login&outputType=json"
-    self.body_login = { "serviceName": "MobileLoginSP.login", "requestBody": { "NOMUSU": { "$": "SUP" }, "INTERNO":{ "$":"$CB!vy8@ZO" }, "KEEPCONNECTED": { "$": "S" } } }
+    self.body_login = { "serviceName": "MobileLoginSP.login", "requestBody": { "NOMUSU": { "$": ""+ self.user +"" }, "INTERNO":{ "$":""+ self.password +"" }, "KEEPCONNECTED": { "$": "S" } } }
     self.headers_login = {'Content-type': 'application/json', 'Accept': 'text/plain'}
     self.url_logout = "http://navecunha.nuvemdatacom.com.br:9665/mge/service.sbr?serviceName=MobileLoginSP.logout&outputType=json"
     self.body_logout = {"serviceName":"MobileLoginSP.logout","status":"1","pendingPrinting":"false"}
